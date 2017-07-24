fadeafde38ec9d42aa4d8eedea4525dc84a92989
TrevKnows <Trevor.Beaton11@gmail.com>
2017-07-24 12:33:50 -0400

    Initial Commit

---
+++ IO Connect Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IO Connect Example
+//
+//  Created by Trevor Beaton on 7/24/17.
+//  Copyright © 2017 Vanguard Logic LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
