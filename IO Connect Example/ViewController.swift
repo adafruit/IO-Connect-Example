4deff8759c5a0bdb22d6a7618b8e3554a0d3eabe
TrevKnows <Trevor.Beaton11@gmail.com>
2017-07-24 15:53:09 -0400

    working

--- IO Connect Example/ViewController.swift
+++ IO Connect Example/ViewController.swift
@@ -7,19 +7,98 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    
+    var motionManager = CMMotionManager()
+    
+    @IBOutlet weak var accelTagX: UILabel!
+    
+    @IBOutlet weak var accelSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startAccelerometerX()
+        self.accelTagX.text = "--"
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func startAccelerometerX () {
+        print("Start Acceleromter Updates")
+        motionManager.accelerometerUpdateInterval = 2.5
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: {
+            (accelerData:CMAccelerometerData?, error: Error?) in
+            if (error != nil ) {
+                print("Error")
+            } else {
+                
+                let accelX = accelerData?.acceleration.x
+                self.accelTagX.text = String(format: "%.02f", accelX!)
+                self.postAccelerometerDataX()
+                print("Accelerometer X: \(accelX!)")
+            }
+        })
     }
 
+    func stopAccelerometerX () {
+        self.motionManager.stopAccelerometerUpdates()
+        self.accelTagX.text = "--"
+        print("Accelerometer X Stopped")
+    }
+
+    
+    
+    
+    
+    
+    /*
+     //for accelerometer x
+     @IBAction func stateChange(_ sender: UISwitch) {
+     if (sender.isOn == true){
+     self.aSwitchY.setOn(false, animated: true)
+     self.aSwitchZ.setOn(false, animated: true)
+     startAccelerometerX()
+     
+     }else {
+     stopAccelerometerX()
+     }
+     }
+     
+
+ */
+    
+    func postAccelerometerDataX() {
+        
+        let parameters = ["value": "\(String(format: "%.02f", (motionManager.accelerometerData?.acceleration.x)!))"]
+        guard let url = URL(string: "https://io.adafruit.com/api/feeds/your-Feed-Key-Here/data.json?X-AIO-Key=Your-A-IO-Key-Here") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+            }.resume()
+    }
+
+    
+    
 
 }
 
